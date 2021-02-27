244cadb554414dc9194774aa64a917c6a54144d9
Piotr Kaminski <piterwk1@.wp.pl>
2021-02-27 09:25:50 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,6 @@
 /* eslint-disable react/destructuring-assignment */
 import React, { Component } from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import SignIn from './components/auth/SignIn';
-import SignUp from './components/auth/SignUp';
-import Dashboard from './components/dashboard/Dashboard';
-import Navbar from './components/layout/Navbar';
-import CreateProject from './components/projects/CreateProject';
-import ProjectDetails from './components/projects/ProjectDetails';
+import Navbar from './components/Navbar';
 
 class App extends Component {
   // eslint-disable-next-line react/state-in-constructor
@@ -14,18 +8,9 @@ class App extends Component {
 
   render() {
     return (
-      <BrowserRouter>
-        <div className="App">
-          <Navbar />
-          <Switch>
-            <Route path="/" exact component={Dashboard} />
-            <Route path="/project/:id" component={ProjectDetails} />
-            <Route path="/signin" component={SignIn} />
-            <Route path="/signup" component={SignUp} />
-            <Route path="/create" component={CreateProject} />
-          </Switch>
-        </div>
-      </BrowserRouter>
+      <div className="App">
+        <Navbar />
+      </div>
     );
   }
 }
